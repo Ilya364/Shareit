c617540b92249b729d37d7fc4369d68e759af1e7
ilya364 <tsh335nextYeAr@yandex.ru>
2024-03-31 21:18:26 +0300

    feat:добавлены контроллеры, сервисы, репо для Item и User

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,27 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class Item {
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    private Long owner;
+
+    public Item(Long id, String name, String description, Boolean available) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.available = available;
+    }
 }
